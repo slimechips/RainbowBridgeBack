e5c75badb53a554f3350b1a29f232c5a89092dab
slimechips <jason.chowcs#gmail.com>
2020-03-10 11:15:26 +0800

    add category, status, support req models

--- web-svc/controllers/webroute.ts
+++ web-svc/controllers/webroute.ts
@@ -1,11 +1,10 @@
 import { Response, Request, Router } from 'express';
-import { RainbowSDK } from 'rainbow-node-sdk';
 
 // Init router here
 export const router = Router();
 
 export const getLogin = (req: Request, res: Response) => {
-  res.status(200).json({});
+  res.status(200).json({ hello: 'worldlllll' });
 };
 
 export const hello = 'l';
---
+++ web-svc/models/ReqStatus.ts
@@ -0,0 +1,7 @@
+export enum ReqStatus {
+  New,
+  Queued,
+  Accepted,
+  Cancelled,
+  Completed,
+}
